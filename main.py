a688758f46497cd8ae7f982fa443e220b68b44e7
Vasilis <bouzasbasilis@hotmail.com>
2021-07-14 18:14:47 +0300

    compute increase / decrease rate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from variables import *
+import variables as var
 import pandas as pd
 import geopandas as gpd
 from src import actions
@@ -10,8 +10,8 @@ def get_flows():
     Import flows from 'data/flows'
     """
     flows = []
-    for year in YEARS:
-        filename = f'./data/flows/ontvangst_{PROVINCE.lower()}_{year}.csv'
+    for year in var.YEARS:
+        filename = f'./data/flows/ontvangst_{var.PROVINCE.lower()}_{year}.csv'
         csv = pd.read_csv(filename, low_memory=False)
         flows.append(csv)
 
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     logging.info("INPUTS...")
     logging.info("Load flows...")
     # flows = get_flows()
-    flows = pd.read_csv(f'./data/flows/ontvangst_{PROVINCE.lower()}.csv', low_memory=False)
+    flows = pd.read_csv(f'./data/flows/ontvangst_{var.PROVINCE.lower()}.csv', low_memory=False)
     flows['Gewicht_TN'] = flows['Gewicht_KG'] / 10**3
     logging.info(f"Total flows: {len(flows)}")
 
--- src/actions.py
+++ src/actions.py
@@ -1,10 +1,9 @@
-from variables import *
+import variables as var
 import pandas as pd
 import geopandas as gpd
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import logging
-import math
 
 
 def join_areas(flows, areas=None, role=None, admin_level=None):
@@ -27,10 +26,78 @@ def join_areas(flows, areas=None, role=None, admin_level=None):
     return flows
 
 
-def compute_trends():
+def compute_trends(df, on=None, values=[], per_months=12):
     """
-    Compute trends via Linear Regression
+    Analyse trends for areas on different timeframe
+    :param on: name property for area
+    :param values: select specific values for on-property
+    :param per_months: select timeframe in months (year: 12 months, quarter: 3 months etc.)
+    :return:
     """
+
+    print(on)
+
+    df['Verwerking'] = df['VerwerkingsmethodeCode'].str[0]
+
+    # columns to select & groupby
+    columns = [
+        on,
+        'Verwerking',
+        'MeldPeriodeJAAR',
+        'Gewicht_TN'
+    ]
+    groupby = columns[:-1]
+
+    # split months into periods
+    months = list(range(1, 13))
+    periods = [
+        months[i: i + per_months] for i in range(0, len(months), per_months)
+    ]
+
+    # aggregate into periods of months for each year
+    df_new = []
+    for year in var.YEARS:
+        # aggregate per period & store to new df
+        for idx, period in enumerate(periods):
+            df_q = df[
+                (df['MeldPeriodeJAAR'] == year) &
+                (df['MeldPeriodeMAAND'].isin(period))
+                ]
+            df_q = df_q[columns].groupby(groupby).sum().reset_index()
+            df_q['Periode'] = idx + 1
+            df_new.append(df_q)
+
+    # concatenate for all periods & years
+    df_new = pd.concat(df_new)
+
+    # # find unique values for on-property
+    if not values:
+        values = df_new.drop_duplicates(on)
+        values = values[on].to_list()
+
+    # retrieve flows for each unique values & run analysis
+    with open(f'./test/{on}_trends.csv', mode='w') as file:
+        file.write(f'{on}\n')
+        for value in values:
+            flows = df_new.loc[df_new[on] == value]
+            X, Y = [], []
+            for idx, flow in flows.iterrows():
+                time = flow['MeldPeriodeJAAR'] * 12 + flow['Periode'] * per_months
+                amount = flow['Gewicht_TN']
+                X.append(time)
+                Y.append(amount)
+            X = np.array(X).reshape(-1, 1)
+            if Y:
+                reg = LinearRegression().fit(X, Y)
+                # return:
+                # 1) sign of alpha coefficient (+: increase, -: decrease)
+                # 2) increase/decrease rate ???
+                # 3) extrapolation ???
+                Y_initial = reg.predict(np.array(X[0]).reshape(-1, 1))[0]
+                Y_final = reg.predict(np.array(X[-1]).reshape(-1, 1))[0]
+                sign = reg.coef_[0] / abs(reg.coef_[0])
+                change = (Y_final - Y_initial) / Y_initial * 100
+                file.write(f'{value}: {Y_initial}, {Y_final}, {change:.1f}%\n')
 
 
 def compute_actions(flows, provincies, gemeenten):
@@ -57,67 +124,11 @@ def compute_actions(flows, provincies, gemeenten):
     # ]
 
     # get names of provincie gemeenten
-    provincie_gemeenten = gemeenten[gemeenten['parent'] == PROVINCE]['name'].to_list()
+    provincie_gemeenten = gemeenten[gemeenten['parent'] == var.PROVINCE]['name'].to_list()
 
-    # linear regression
-    def linear_regression(df, on=None, values=[], per_months=12):
-        print(on)
-
-        # aggregate into periods of months for each year
-        df_new = []
-        for year in YEARS:
-            # columns to select & groupby
-            columns = [
-                on,
-                'MeldPeriodeJAAR',
-                'Gewicht_TN'
-            ]
-            groupby = columns[:-1]
-
-            # split months into periods
-            months = list(range(1, 13))
-            periods = [
-                months[i : i + per_months] for i in range(0, len(months), per_months)
-            ]
-
-            # aggregate per period & store to new df
-            for idx, period in enumerate(periods):
-                df_q = df[
-                    (df['MeldPeriodeJAAR'] == year) &
-                    (df['MeldPeriodeMAAND'].isin(period))
-                ]
-                df_q = df_q[columns].groupby(groupby).sum().reset_index()
-                df_q['Periode'] = idx + 1
-                df_new.append(df_q)
-
-        # concatenate for all periods & years
-        df_new = pd.concat(df_new)
-
-        # # find unique values for on-property
-        if not values:
-            values = df_new.drop_duplicates(on)
-            values = values[on].to_list()
-
-        # retrieve flows for each unique values & run analysis
-        with open(f'./test/{on}_{per_months}.csv', mode='a') as file:
-            file.write('area\tslope\n')
-            for value in values:
-                flows = df_new.loc[df_new[on] == value]
-                # X, Y = [], []
-                # for idx, flow in flows.iterrows():
-                #     time = flow['MeldPeriodeJAAR'] * 12 + flow['Periode'] * per_months
-                #     amount = flow['Gewicht_TN']
-                #     X.append(time)
-                #     Y.append(amount)
-                # X = np.array(X).reshape(-1, 1)
-                # if Y:
-                #     reg = LinearRegression().fit(X, Y)
-                #     print(value, math.degrees(math.atan(reg.coef_[0])))
-                #     file.write(f'{value}\t{math.degrees(math.atan(reg.coef_[0]))}\n')
-                flows.to_csv(f'./test/{on}_{value}_{per_months}.csv', index=False)
-
-    # trend analysis on origin provincies (herkomst)
-    # linear_regression(flows, on='Herkomst_Provincie', values=[PROVINCE])
-    # linear_regression(flows, on='Verwerker_Provincie', values=[PROVINCE])
-    linear_regression(flows, on='Herkomst_Gemeente', values=provincie_gemeenten, per_months=12)
-    # linear_regression(flows, on='Verwerker_Gemeente', values=provincie_gemeenten, per_months=12)
+    # compute trends for origin/destination areas
+    logging.info("Compute trends for areas...")
+    # compute_trends(flows, on='Herkomst_Provincie', values=[var.PROVINCE])
+    # compute_trends(flows, on='Verwerker_Provincie', values=[var.PROVINCE])
+    compute_trends(flows, on='Herkomst_Gemeente', values=provincie_gemeenten, per_months=3)
+    compute_trends(flows, on='Verwerker_Gemeente', values=provincie_gemeenten, per_months=3)
